bc47ebb3548bbd342d666220f4c217cd5cd1473e
galvin-SH <mgalvin1993@gmail.com>
2023-11-02 00:53:45 -0700

    initial setup and routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction,
+} = require("../../controllers/thought-controller");
+
+router.route("/").get(getAllThoughts).post(createThought);
+
+router
+    .route("/:id")
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought)
+    .put(addReaction)
+    .delete(deleteReaction)
+    .delete(deleteThought);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require("../../controllers/user-controller");
+
+router.route("/").get(getAllUsers).post(createUser);
+
+router
+    .route("/:id")
+    .get(getUserById)
+    .put(updateUser)
+    .put(addFriend)
+    .delete(deleteFriend)
+    .delete(deleteUser);
+
+module.exports = router;
